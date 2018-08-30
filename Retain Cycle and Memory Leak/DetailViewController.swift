a43bc283dbb965dce301c6d27dcae1020e6523c4
Spoke <spoke@Spokede-MacBook-Air.local>
2018-08-30 17:56:59 +0800

    Start

--- Retain Cycle and Memory Leak/DetailViewController.swift
+++ Retain Cycle and Memory Leak/DetailViewController.swift
@@ -8,34 +8,32 @@
 
 import UIKit
 
-protocol DataModelDelegate: class {
-    func didPassData(data: String)
-}
+//protocol DataModelDelegate: class {
+//    func didPassData(data: String)
+//}
 
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
-       weak var delegate: DataModelDelegate? {
-        didSet {
-            print("Ya")
-            print(self.delegate)
-        }
-    }
+//       weak var delegate: DataModelDelegate? {
+//        didSet {
+//            print("Ya")
+//            print(self.delegate)
+//        }
+//    }
+    
+    var completionHandler: ((_ data: String) -> Void)?
     
 
     deinit {
-        if self.delegate == nil {
-            print("VC1 is bring removed from memory")
-        }
         print("----------------------")
         print("VC2 is bring removed from memory")
     }
     
     func requestData() {
         let data = "Data from VC2"
-        delegate?.didPassData(data: data)
-        
+        completionHandler!(data)
     }
 
     override func viewDidLoad() {
